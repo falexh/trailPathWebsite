6bcb89b2b5391bfb860d408051ca8327d6d6c0e9
falexh <hellardf@oregonstate.edu>
2018-09-25 16:10:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var x = console.log("Page has successfully loaded");
+x;
+
+var buttonMaker = document.getElementById('dlBody');
+buttonMaker.innerHTML = 'OR...';
+
+var timeMaker = document.getElementById('dlTime');
+timeMaker.innerHTML = 'The current time is: ' + (Date());
+
+var getOut = document.getElementById('getOut');
+getOut.innerHTML = 'Time to get on the trail!';
